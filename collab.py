b476c2790ce7b7f19cce53a4661216f8094e2530
BIKASH-ReX-TRACY <130630071+BIKASH-ReX-TRACY@users.noreply.github.com>
2023-05-05 13:50:55 +0545

    Create collab.py

---
+++ collab.py
@@ -0,0 +1,25 @@
+#decode by : BIKASH MAGAR 
+
+#created by LAHUREY 
+
+#tottaly written by bikash
+
+#!/usr/bin/python3
+
+#---------------------[IMPORT]---------------------#
+
+from bs4 import BeautifulSoup as sop
+
+from concurrent.futures import ThreadPoolExecutor as tred
+
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+
+import requests,random,sys,json,os,re
+
+from time import sleep
+
+from os import system
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+
+import os,sys,time,json,random,re,string,platform,base64,uuid
