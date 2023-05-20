6e2423b7f20850b5d6a415a616d62fbb112f9e36
BIKASH-ReX-TRACY <130630071+BIKASH-ReX-TRACY@users.noreply.github.com>
2023-05-20 16:46:22 +0545

    Create collab.py

---
+++ collab.py
@@ -0,0 +1,15 @@
+import os,platform
+
+os.system('git pull')
+
+ 
+
+trt=platform.architecture()[0]
+
+if trt=="32bit":
+
+    print('Sorry 32 Bit Not Supported...')
+
+elif trt=="64bit":
+
+    __import__("collab")
